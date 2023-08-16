cc5184c37a1bf2125cbc9a6701e359e133a922cf
Anmol Singh <ASingh2320@outlook.com>
2023-08-16 05:50:11 -0700

    most of auth front end built

--- client/src/App.js
+++ client/src/App.js
@@ -8,27 +8,38 @@ import {
 import NavBar from './components/Navbar';
 import Home from './components/Home';
 import LoginSignup from './components/LoginSignup';
+import Register from './components/Register';
 import Discover from './components/Discover';
 import CourtDetail from './components/CourtDetail';
 import AddCourt from './components/AddCourt';
 import AddReview from './components/AddReview';
 import Profile from './components/Profile';
 
+import './App.css';
+
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" exact element={<Home/>} />
-        <Route path="/login" element={<LoginSignup/>} />
-        <Route path="/discover" element={<Discover/>} />
-        <Route path="/court/:id" element={<CourtDetail/>} />
-        <Route path="/add-court" element={<AddCourt/>} />
-        <Route path="/add-review/:courtId" element={<AddReview/>} />
-        <Route path="/profile" element={<Profile/>} />
-      </Routes>
-    </Router>
-    
+    <div className="App">
+      <Router>
+
+        {/* Commented out navbar because there was not styling
+        <NavBar /> */}
+        <Routes>
+          {/* For now just had home go to login screen
+          TODO: write logic to go to home screen when not
+          logged in else go to home */}
+          <Route path="/" exact element={<LoginSignup/>} />
+          <Route path="/login" element={<LoginSignup/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/discover" element={<Discover/>} />
+          <Route path="/court/:id" element={<CourtDetail/>} />
+          <Route path="/add-court" element={<AddCourt/>} />
+          <Route path="/add-review/:courtId" element={<AddReview/>} />
+          <Route path="/profile" element={<Profile/>} />
+        </Routes>
+      </Router>
+      
+    </div>
   );
 }
 
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom';
+import "../App.css"
 
 export default function LoginSignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
 
   const navigate = useNavigate()
 
@@ -13,21 +15,23 @@ export default function LoginSignUp() {
     console.log(password);
   }
 
-  const toRegister = () => {
-    navigate("/register");
+  const toLogin = () => {
+    navigate("/login");
   }
 
   return (
     <div className="auth-container">
-       <h2>Login</h2>
-      <form className="login-form" onSubmit={handleSubmit}>
+      <h2>Register</h2>
+      <form className="register-form" onSubmit={handleSubmit}>
         <label htmlFor="email">email</label>
         <input value={email} onChange={(e) => setEmail(e.target.value)} type='email' placeholder='Please enter email' id="email" name="email"/>
         <label htmlFor="password">password</label>
         <input value={password}  onChange={(e) => setPassword(e.target.value)} type='password' placeholder='Please enter password' id="password" name="password"/>
-        <button>Login</button>
+        <label htmlFor="name">name</label>
+        <input value={name}  onChange={(e) => setName(e.target.value)} type='name' placeholder='Please enter name' id="name" name="name"/>
+        <button>Register</button>
       </form>
-      <button onClick={toRegister}className="auth-button">Register</button>
+      <button onClick={toLogin} className="auth-button">Login</button>
     </div>
   )
 }
