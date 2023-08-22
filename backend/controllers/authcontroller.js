36847d3cd9724e1e43e95023a26e21a08d36fac8
Anmol Singh <ASingh2320@outlook.com>
2023-08-21 23:37:51 -0700

    added login and user verification

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -20,4 +20,31 @@ module.exports.SignUp = async (req, res, next) => {
     });
 
     res.status(200).json({ message: "User signed in", success: true, user});
+    next();
 }
+
+module.exports.Login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if(!email || !password ){
+            return res.json({message:"All fields are required"})
+        }
+        const user = await User.findOne({ email });
+        if(!user){
+            return res.json({message:"Incorrect password or email" }) 
+        }
+        const auth = await bcrypt.compare(password,user.password)
+        if (!auth) {
+            return res.json({message:"Incorrect password or email" }) 
+        }
+        const token = generateToken(user._id);
+        res.cookie("token", token, {
+            withCredentials: true,
+            httpOnly: false,
+        });
+        res.status(200).json({ message: "User logged in", success: true });
+        next()
+    } catch (error) {
+        console.error(error);
+    }
+  }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,10 @@
-const { SignUp } = require("../controllers/authcontroller");
+const { SignUp, Login } = require("../controllers/authcontroller");
+const { userVerify } = require("../middleware/auth");
 const express = require('express');
 const router = express.Router();
 
+router.post('/', userVerify)
 router.post("/signup", SignUp);
+router.post("/login", Login);
 
 module.exports = router;
